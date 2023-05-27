c5a16225401537fd6305e8458bcce2163f7b1fb2
gowrishankarraman <rgs5045@gmail.com>
2023-05-27 21:38:27 +0530

    thirdd commit

--- Test.js
+++
@@ -1,8 +0,0 @@
-const http=require('http')
-const { hostname } = require('os')
-const server=http.createServer((req,res)=>{
-    res.write("hai the web page")
-    res.end()
-})
-
-server.listen(5000,()=>console.log(`server port no ${hostname}`))
